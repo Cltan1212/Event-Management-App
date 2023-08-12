6c744295653af658cdcf8460635654d95d9588fa
uvuu0001 <uvuu0001@student.monash.edu>
2023-08-12 23:05:56 +1000

    add get/post for add-category & other stuffs

--- routers/category.js
+++ routers/category.js
@@ -1,30 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const Category = require("../models/event-category.js");
 
-let categoryDb = [];
+let categoryDB = [];
 
-router.get("/", function(req, res) { //maybe return to all categories
+router.get("/", function(req, res) { // WIP maybe return to all categories
     res.sendFile(path.join(__dirname, "../views",'index.html'));
 });
 
+//---------------------------------Add Category feature---------------------------------
+
+// Handle GET request to show the "Add Category" form
 router.get("/add-category", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views", "category-add.html"));
+});
+
+// Handle POST request when the form is submitted
+router.post("/add-category", function(req, res) {
+    console.log (req.body.name);
+    let description = req.body.description;
+    let image = req.body.image;
+
+    let category1 = new Category(name);
+
+    categoryDB.push(category1);
+
+    res.redirect('/event-categories')
+});
+
+// ---------------------------------List all categories in tabular format---------------------------------
+router.get("/event-categories", function(req, res) { // WIP
+    res.send(categoryDB);
+});
+
+router.get("/search-category", function(req, res) { // WIP
     res.sendFile(path.join(__dirname, "../views",'index.html'));
 });
 
-router.get("/event-categories", function(req, res) {
+router.get("/event", function(req, res) { // WIP
     res.sendFile(path.join(__dirname, "../views",'index.html'));
 });
 
-router.get("/search-category", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views",'index.html'));
-});
-
-router.get("/event", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views",'index.html'));
-});
-
-router.get("/delete-category", function(req, res) {
+router.get("/delete-category", function(req, res) { // WIP
     res.sendFile(path.join(__dirname, "../views",'index.html'));
 });
 
--- server.js
+++ server.js
@@ -7,11 +7,19 @@ const morgan = require("morgan"); // middleware
 const app = express();
 
 // Importing all the routes
-const routerCategory = require("./routers/category.js");
+const routerCategory = require("./routers/category.js"); // import the category router
 const eventRoute = require('./routers/event'); // import event's router
 
+// Config
 app.listen(8080);
 app.use(morgan('tiny'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}));
+// parse application/json
+app.use(express.json())
+
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname,"views","index.html"));
@@ -25,3 +33,18 @@ app.use('/events', eventRoute)
 app.use('/sold-out-events', eventRoute);
 app.use('/categoty/:categoryid', eventRoute);
 app.use('/delete-event', eventRoute);
+
+/** URL Parameters
+ * http://localhost:8080/add/BMW/X7/2023
+ */
+
+/** Query String
+ * http://localhost:8080/add?maker=BMW&model=X7&year=2023
+ */
+
+/** HTTP verbs/ HTTP methods
+ * GET: get data from backend
+ * POST: send data to backend
+ * PUT: to send data to update existing data
+ * DELETE: delete data from backend
+ */
