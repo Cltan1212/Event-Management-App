ffbcba6909d45f6a3c69095f6b081424c1126a62
Cltan1212 <110282763+Cltan1212@users.noreply.github.com>
2023-08-16 12:12:14 +1000

    add bootstrap to each page

--- models/event.js
+++ models/event.js
@@ -1,4 +1,5 @@
 const randString = require("randomstring");
+const { format } = require("date-fns"); // I use this for convert the data format
 
 class Event {
     constructor(
@@ -6,21 +7,30 @@ class Event {
         description = "", 
         startDateTime, 
         duration, 
+        endDateTime,
         isActive = true,
-        image = "",
+        image = "", // later put default pic here
         capacity = 1000,
-        ticketsAvailable = capacity,
         categoryID) {  
         
+        // Parse the startDateTime and endDateTime strings into Date objects
+        const startDate = new Date(startDateTime);
+        const endDate = new Date(endDateTime);
+
+        // Format the Date objects into the desired string format
+        const formattedStartDateTime = format(startDate, "MM/dd/yyyy, h:mm:ss a");
+        const formattedEndDateTime = format(endDate, "MM/dd/yyyy, h:mm:ss a");
+
         this.id = `E${randString.generate({length: 2,charset: "ABCDEF",})}-${Math.round(Math.random()*1000)}`;
         this.name = name;
         this.description = description;
-        this.startDateTime = startDateTime;
-        this.duration = duration;
+        this.startDateTime = formattedStartDateTime;
+        this.duration = duration/60 + " hour(s)";
+        this.endDateTime = formattedEndDateTime ;
         this.isActive = isActive;
-        this.image = image; // later put default pic here
+        this.image = image; 
         this.capacity = capacity;
-        this.ticketsAvailable = ticketsAvailable;
+        this.ticketsAvailable = capacity;
         this.categoryID = categoryID;
     }
 }
--- routers/event.js
+++ routers/event.js
@@ -5,35 +5,52 @@ let Event = require("../models/event");
 
 let events = [];
 
+// Example event data
+const event1 = new Event("Example Event 1",
+    "This is the first example event.",
+    "2023-08-20T15:00",
+    120,
+    "2023-08-20T17:00",
+    true,
+    "example-image-1.jpg",
+    100,
+    "cat-1"
+    )
+
+events.push(event1);
+
 eventRoute.get('/add-event', function(req, res) {
     res.sendFile(path.join(__dirname, "../views", "/event-add.html"));
 });
 
 eventRoute.post('/add-event', function(req, res) {
     const eventData = req.body;
+
+    // Calculate endDateTime based on startDateTime and duration
+    const startDateTime = new Date(eventData.startDateTime);
+    const duration = parseInt(eventData.duration);
+    const endDateTime = new Date(startDateTime.getTime() + (duration * 60000)); // Convert duration to milliseconds
+
     const newEvent = new Event(
         eventData.name,
-        eventData.description,
+        eventData.description, 
+        startDateTime, 
+        duration, 
+        endDateTime,
+        eventData.isActive,
         eventData.image,
-        eventData.startDateTime,
-        eventData.duration,
-        eventData.isActive === 'on', // Convert checkbox value to a boolean
         eventData.capacity,
-        eventData.ticketsAvailable,
         eventData.categoryID
     )
     events.push(newEvent);
+    console.log(events)
     res.redirect(path.join(req.baseUrl, '/events'));
 })
 
 eventRoute.get('/events', function(req, res) {
-    res.render('event-list', { events: events });
+    res.render('event-list', { name: "Event List", events: events });
     res.sendFile(path.join(__dirname, "../views", "event-list.html"));
 })
-
-eventRoute.get('/sold-out-events', function(req, res) {
-    res.sendFile(path.join(__dirname, "../views", "event-delete.html"))
-});
 
 eventRoute.get('/categoty/:categoryid', function(req, res) {
     res.sendFile(path.join(__dirname, "../views", "category.html"))
@@ -62,6 +79,15 @@ eventRoute.get('/delete-event', function(req, res) {
 eventRoute.get('/delete', function(req, res) {
     res.render('event-delete', { events: events });
     res.sendFile(path.join(__dirname, "../views", "event-delete.html"))
+})
+
+/**
+ * Sold out event
+ */
+eventRoute.get('/sold-out-events', function(req, res) {
+    let soldOutList = events.filter((event) => event.ticketsAvailable == 0);
+    res.render('event-list', { name:"Sold Out List", events: soldOutList });
+    res.redirect("/ChunLing/events");
 })
 
 eventRoute.post('/delete-event', function(req, res) {
